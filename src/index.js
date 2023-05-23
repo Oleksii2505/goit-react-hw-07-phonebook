7cc4f512fd67b2fa7973c79aeff9f342be4c2c56
Oleksii Puzanov <alexey.puzanov@i.ua>
2023-05-23 21:55:58 +0300

    Add operations.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { App } from 'App';
-import { store, persistor } from 'components/Redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from 'components/Redux/store';
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  <React.StrictMode>
     <Provider store = {store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>   
+        <App />  
     </Provider>
   </React.StrictMode>
 );
-// https://6468ee7703bb12ac20823844.mockapi.io/api/contacts/:endpoint
